fb38c7d7de8880ecffb2bdc0a0e157a5c326b318
alex23job <alex23job@yandex.ru>
2023-08-08 12:29:15 +0300

    final

--- Modul2HomeWork1ConsoleApp/Program.cs
+++ Modul2HomeWork1ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace Modul2HomeWork1ConsoleApp
     {
         static void Main(string[] args)
         {
+            string fullName = "Петров Андрей Павлович";
+            int age = 38;
+            string email = "PetrowAP@mail.ru";
+            float programmingScores = 4.9f;
+            float mathScores = 4.6f;
+            float physicsScores = 4.3f;
+            string scorePattern = "{0, -20} : {1:0.00}";
+            string scorePatternNewLine = "\n{0, -20} : {1:0.00}";
+
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Ф.И.О.  : " + fullName);
+            Console.WriteLine("Возраст : {0}", age);
+            Console.WriteLine("E-mail  : " + email);
+            Console.WriteLine(scorePatternNewLine, "Программирование", programmingScores);
+            Console.WriteLine(scorePattern, "Математика", mathScores);
+            Console.WriteLine(scorePattern, "Физика", physicsScores);
+
+            Console.ReadKey();
         }
     }
 }
--- Modul2HomeWork2ConsoleApp/Program.cs
+++ Modul2HomeWork2ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,36 @@ namespace Modul2HomeWork2ConsoleApp
     {
         static void Main(string[] args)
         {
+            string fullName = "Иванов Дмитрий Сергеевич";
+            int age = 47;
+            string email = "IvanowDS@mail.ru";
+            float programmingScores = 4.7f;
+            float mathScores = 4.9f;
+            float physicsScores = 4.5f;
+            string scorePattern = "{0, -20} : {1:0.00}"; 
+            string scorePatternNewLine = "\n{0, -20} : {1:0.00}";
+
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Ф.И.О.  : " + fullName);
+            Console.WriteLine("Возраст : {0}", age);
+            Console.WriteLine("E-mail  : " + email);
+            Console.WriteLine(scorePatternNewLine, "Программирование", programmingScores);
+            Console.WriteLine(scorePattern, "Математика", mathScores);
+            Console.WriteLine(scorePattern, "Физика", physicsScores);
+
+            Console.ReadKey();
+
+            float sumOfBalls = 0;
+            float averageScore = 0;
+
+            sumOfBalls = programmingScores + mathScores + physicsScores;
+            averageScore = sumOfBalls / 3;
+
+            Console.WriteLine(scorePatternNewLine, "Сумма баллов", sumOfBalls);
+            Console.WriteLine(scorePattern, "Средний балл", averageScore);
+
+            Console.ReadLine();
         }
     }
 }
